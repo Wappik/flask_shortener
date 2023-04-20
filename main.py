f24a833c0b1878735fa35da8a84efed38acdc107
Wappik <wappik4@gmail.com>
2023-04-20 19:16:43 +0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+# TODO: Home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# TODO: Urls page
+@app.route('/urls')
+def urls():
+    return render_template('urls.html')
+    # pass
+
+
+# TODO: Redirect page
+@app.route('/<short>')
+def url_redirect(short):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
