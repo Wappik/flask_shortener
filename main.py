4a34f2d80074206e93d5feca8cae348a064ce406
Misha <pugin_2015@mail.ru>
2023-04-22 17:10:09 +0300

    Url_info_db

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
+class UrlInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(255), unique=True, nullable=False)
+    short = db.Column(db.String(6), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    created_date = db.Column(db.DateTime, default=datetime.now)
+
+db.create_all()
 
 # TODO: Home page
 @app.route('/')
